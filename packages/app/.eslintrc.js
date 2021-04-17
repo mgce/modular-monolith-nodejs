61cc6c7d7824c6704e6fe7f44424a310e46deb94
Mateusz Gajda <mateusz.gajda@tsh.io>
2021-04-17 14:54:41 +0200

    remove unused packages

--- packages/app/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-require('@travelhoop/toolchain/patch/modern-module-resolution');
-
-module.exports = {
-  extends: [ "./node_modules/@travelhoop/toolchain/includes/.eslintrc" ],  // <---- put your profile string here
-  parserOptions: { 
-    project: "tsconfig.json",
-    tsconfigRootDir: __dirname 
-  }
-};
